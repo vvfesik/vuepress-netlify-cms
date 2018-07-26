8170b239deb10f54e24c34adc3e63333deac9290
Vasyl Fesik <vasyl.fesik@verygoodsecurity.com>
2018-07-26 19:49:02 +0300

    test update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,22 @@
+const mainMenuRaw = require('../.vuepress/mainmenu.json').menu;
+let newMenu = [];
+mainMenuRaw.forEach(el => {
+  newMenu.push(el.document)
+});
+
 module.exports = {
   title: 'Netlify CMS + VuePress',
   description: 'Netlify + VuePress',
   themeConfig: {
     docsDir: 'docs',
     repo: 'vvfesik/vuepress-netlify-cms',
-    sidebar: [
-      '/',
-      '/welcome',
-      '/test'
-    ],
+    // sidebar: [
+    //   '/',
+    //   '/welcome',
+    //   '/test'
+    // ],
+    sidebarItems: newMenu,
+    sidebar: mainMenuRaw,
     nav: [
       {
         text: 'Admin',
